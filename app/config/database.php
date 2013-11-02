abd9014cd10b48dcd640142af16e88bc8b485c5c
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-02 18:53:46 +0000

    DB database config

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'git-stars',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
