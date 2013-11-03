d1ff8986d327e8c452b1e9a68c68264b892828ba
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-03 19:27:43 +0000

    Modified controller to loop through multiple users, plus some cleaning up

--- app/controllers/GithubController.php
+++ app/controllers/GithubController.php
@@ -13,31 +13,38 @@ class GithubController extends BaseController {
 
 	public function update()
 	{
+		// Base github url
 		$github_url = 'https://api.github.com/users/';
 
-		// $users = [
-		// 	'40thieves',
-		// 	'edpoole',
-		// 	'rmlewisuk',
-		// 	'danharper',
-		// ];
+		// List of users to index
+		$users = [
+			'40thieves',
+			'edpoole',
+			'rmlewisuk',
+			'danharper',
+		];
 
-		// Uses Requests library to make http request
-		$user_response = Requests::get($github_url . '40thieves');
-		$user_json = json_decode($user_response->body);
-
-		// Updates user model
-		$user = User::createIfDoesNotExist($user_json->login);
-
-		// Uses Requests library to make http request
-		$stars_response = Requests::get($github_url . '40thieves/starred');
-		$stars_json = json_decode($stars_response->body);
-
-		foreach($stars_json as $star)
+		foreach($users as $user)
 		{
-			$repo = Repo::createIfDoesNotExist($star->name, ['language' => $star->language]);
+			// Uses Requests library to make http request
+			$user_response = Requests::get($github_url . $user);
+			$user_json = json_decode($user_response->body);
 
-			$foo = Star::createIfDoesNotExist(['user_id' => $user->id, 'repo_id' => $repo->id]);
+			// Updates user model
+			$user_model = User::createIfDoesNotExist($user_json->login);
+
+			// Uses Requests library to make http request
+			$stars_response = Requests::get($github_url . $user . '/starred');
+			$stars_json = json_decode($stars_response->body);
+
+			foreach($stars_json as $star)
+			{
+				// Updates repo model
+				$repo_model = Repo::createIfDoesNotExist($star->name, ['language' => $star->language]);
+
+				// Updates star model
+				$star_model = Star::createIfDoesNotExist(['user_id' => $user_model->id, 'repo_id' => $repo->id]);
+			}
 		}
 	}
 
