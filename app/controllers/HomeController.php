82c256f88b8c2233a422b3686b84bec08c7961fe
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-08 12:25:10 +0000

    Start on recommendation, and some tidying up

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -7,17 +7,16 @@ class HomeController extends BaseController {
 	| Default Home Controller
 	|--------------------------------------------------------------------------
 	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
+	| 
 	|
 	*/
 
-	public function showWelcome()
+	public function recommend($repoName)
 	{
-		return View::make('hello');
+		$item = Repo::getFirstByName($repoName);
+		$items = Repo::getAllExceptId($item->id);
+
+		
 	}
 
 }
