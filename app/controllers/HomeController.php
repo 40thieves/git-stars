ba7440882b9cfec38a36efc12e5e1369cea8ca0c
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-08 20:25:29 +0000

    Implements cosine similarity comparison of selected repo with all other repos

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -11,9 +11,82 @@ class HomeController extends BaseController {
 
 	public function recommend($repoName)
 	{
-		list($repo, $allRepos) = Repo::getAllWithSelectedByName($repoName);
+		list($selectedRepo, $allRepos) = Repo::getAllWithSelectedByName($repoName);
 
-		$repoMatrix = Star::generateMatrixByRepoId($repo->id);
+		// Get matrix of stars for selected repo
+		$selectedRepoMatrix = Star::generateMatrixByRepoId($selectedRepo->id);
+		// Calculate magnitude for selected repo
+		$selectedRepoMagnitude = $this->magnitude($selectedRepoMatrix);
+
+		$similarityIndex = [];
+		foreach($allRepos as $repoKey => $repo)
+		{
+			// Get matrix of stars for other repo
+			$repoMatrix = Star::generateMatrixByRepoId($repo->id);
+			// Calculate magnitude for other repo
+			$repoMagnitude = $this->magnitude($repoMatrix);
+
+			$similarityIndex[$repoKey] = $this->similarity($selectedRepoMatrix, $repoMatrix, $selectedRepoMagnitude, $repoMagnitude);
+		}
+
+		// Sort high to low
+		arsort($similarityIndex);
+
+		Log::debug(print_r($similarityIndex, true));
+	}
+
+	/**
+	 * Calculates dot product of two given matrices
+	 * @param  array $aMatrix First matrix
+	 * @param  array $bMatrix Second matrix
+	 * @return int            Dot product of matrices
+	 */
+	private function dotProduct($aMatrix, $bMatrix)
+	{
+		$arr = [];
+		foreach($aMatrix as $key => $value)
+		{
+			$arr[$key] =  $value * $bMatrix[$key];
+		}
+		return array_sum($arr);
+	}
+
+	/**
+	 * Calculates magnitude of given matrix
+	 * @param  array $matrix Matrix
+	 * @return float         Magnitude of matrix
+	 */
+	private function magnitude($matrix)
+	{
+		array_map(function($value) {
+			$value = $value * $value;
+		}, $matrix);
+
+		return sqrt(array_sum($matrix));
+	}
+
+	/**
+	 * Calculates cosine similarity for two given matrices
+	 * @param  array $aMatrix    First matrix
+	 * @param  array $bMatrix    Second matrix
+	 * @param  float $aMagnitude (Optional) Calculated magnitude of first matrix
+	 * @param  float $bMagnitude (Optional) Calculated magnitude of second matrix
+	 * @return float             Cosine similarity of matrices
+	 */
+	private function similarity($aMatrix, $bMatrix, $aMagnitude = null, $bMagnitude = null)
+	{
+		// If magnitudes not passed in, calculate
+		if ( ! $aMagnitude) $this->magnitude($aMatrix);
+		if ( ! $bMagnitude) $this->magnitude($bMatrix);
+
+		// Calculate dot product for selected repo and other repo
+		$dotProduct = $this->dotProduct($aMatrix, $bMatrix);
+
+		// Calculate product of selected repo magnitude and other repo magnitude
+		$magnitudeProduct = $aMagnitude * $bMagnitude;
+
+		// Calculate similarity
+		return $dotProduct / $magnitudeProduct;
 	}
 
 }
