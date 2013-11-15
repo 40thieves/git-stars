af854a1416f9fa707922b8a39c92bcd30c875773
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-15 14:08:11 +0000

    Home route, just to show something temporarily

--- app/routes.php
+++ app/routes.php
@@ -7,6 +7,10 @@
 |
 */
 
+Route::get('/', function() {
+	return View::make('hello');
+});
+
 Route::get('recommend/{repoName}', 'HomeController@recommend');
 
 Route::get('github/update', 'GithubController@update');
