cbe7aad99ba5cb0e5aceb1190ba648921e7851ae
Alasdair Smith <alasdairsmith100@gmail.com>
2013-11-22 15:59:38 +0000

    Re-enables error handling

--- app/routes.php
+++ app/routes.php
@@ -24,6 +24,6 @@ Route::get('github', 'GithubController@logInWithGithub');
 Route::get('github/update', 'GithubController@update');
 Route::get('github/foo', 'GithubController@foo');
 
-// App::error(function(Exception $exception) {
-// 	return View::make('error');
-// });
+App::error(function(Exception $exception) {
+	return View::make('error');
+});
